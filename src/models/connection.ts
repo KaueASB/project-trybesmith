19ce47be624062fad80ba11d347ff2b44bf75cd4
Kaue Alves <kaue_alves00@yahoo.com>
2022-07-29 19:53:09 -0300

    reenviando os requisitos para teste no avaliador do github

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,14 +1,10 @@
-import dotenv from 'dotenv';
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { DB_HOSTNAME, DB_USER, DB_PASS } = process.env;
-
-const connection = mysql.createPool({
-  host: DB_HOSTNAME,
-  user: DB_USER,
-  password: DB_PASS,
+export default mysql.createPool({
+  host: process.env.DB_HOSTNAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
 });
-
-export default connection;
